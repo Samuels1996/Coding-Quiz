aa413c821f2f10453ba363765c7066f7c7177bc6
brian samuels <samuels.brian9@gmail.com>
2022-03-27 15:37:36 -0400

    Adding in additonal questions into the js file

--- assets/script.js
+++ assets/script.js
@@ -5,23 +5,32 @@ var currentScore = 0
 var q1 = {
     question: "What CSS value acts as a frame around an image?",
     answers: ["Content", "Margin", "Border", "Padding"],
-    rightAnswer: 2 || index #
+    rightAnswer: 2 || index #,
 }
 
 var q2 = {
     question: "Best practice for console logs is to ____ them to prevent hacking into generator devices", 
-    answers: ["Delete", "Change", "Keep", "Realign"]
-    rightAnswer: 0 || index #
+    answers: ["Delete", "Change", "Keep", "Realign"],
+    rightAnswer: 0 || index #,
 }
 
 var q3 = {
     question: "To move a folder in git you must use",
-    answers: ["rm", "mv", "pull", "mkdir"]
-    rightAnswer: 1 || index #
+    answers: ["rm", "mv", "pull", "mkdir"],
+    rightAnswer: 1 || index #,
 }
 
 var q4 = {
+    question: "What is the base star number for an index?",
+    answers: ["-1", "1", "0", "32"],
+    rightAnswer: 2 || index #,
 
+}
+
+var q5 = {
+    question: "In JavaScript to determine if something is true or false is called a ",
+    answers: ["Array", "Case", "Parse", "Boolean"],
+    rightAnswer: 3 || index #,
 }
 
 function goToQuestions() {
