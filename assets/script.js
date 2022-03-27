754c6ec7ef2aeb0e85dfbae4b883ea9ee315a210
brian samuels <samuels.brian9@gmail.com>
2022-03-27 15:16:33 -0400

    adding in new questions for js file

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,27 @@
 
 var letsPlay = document.querySelector('#button');
 var currentScore = 0
-var q1 =
+var q1 = {
+    question: "What CSS value acts as a frame around an image?",
+    answers: ["Content", "Margin", "Border", "Padding"],
+    rightAnswer: 2 || index #
+}
+
+var q2 = {
+    question: "Best practice for console logs is to ____ them to prevent hacking into generator devices", 
+    answers: ["Delete", "Change", "Keep", "Realign"]
+    rightAnswer: 0 || index #
+}
+
+var q3 = {
+    question: "To move a folder in git you must use",
+    answers: ["rm", "mv", "pull", "mkdir"]
+    rightAnswer: 1 || index #
+}
+
+var q4 = {
+
+}
 
 function goToQuestions() {
 }
