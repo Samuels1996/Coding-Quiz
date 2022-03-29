cfb309ab783f4fb0fece496c441b4059ecfe94c7
Brian Samuels <samuels.brian9@gmail.com>
2022-03-28 20:31:02 -0400

    Adding in additional html, css, as well as js files

--- assets/script.js
+++ assets/script.js
@@ -1,60 +1,66 @@
 var timeEl = document.querySelector(".time");
-
+var buttonEl = ('#button')
 // Below is just setting some base variables that will be used for the JS file
 var letsPlay = document.querySelector('#button');
 var currentScore = 0
 var secondsLeft = 75
 
+
 // Below is going to be the questions and answers for the quiz
-var q1 = {
+const q1 = {
     question: "What CSS value acts as a frame around an image?",
     answers: ["Content", "Margin", "Border", "Padding"],
-    rightAnswer: 2 || index #,
+    rightAnswer: 2,
 }
 
-var q2 = {
+const q2 = {
     question: "Best practice for console logs is to ____ them to prevent hacking into generator devices", 
     answers: ["Delete", "Change", "Keep", "Realign"],
-    rightAnswer: 0 || index #,
+    rightAnswer: 0,
 }
 
-var q3 = {
+const q3 = {
     question: "To move a folder in git you must use",
     answers: ["rm", "mv", "pull", "mkdir"],
-    rightAnswer: 1 || index #,
+    rightAnswer: 1,
 }
 
-var q4 = {
+const q4 = {
     question: "What is the base star number for an index?",
     answers: ["-1", "1", "0", "32"],
-    rightAnswer: 2 || index #,
+    rightAnswer: 2,
 
 }
 
-var q5 = {
+const q5 = {
     question: "In JavaScript to determine if something is true or false is called a(n) ",
     answers: ["Array", "Case", "Parse", "Boolean"],
-    rightAnswer: 3 || index #,
+    rightAnswer: 3,
 }
 
-var q6 = {
+const q6 = {
     question: "In JavaScript you should use local storage for all personal information",
     answers: ["True", "False"],
-    rightAnswer: 0 || index #,
+    rightAnswer: 0,
 }
 
-var q7 = {
+const q7 = {
     question: "Which of these is not used to save a variable",
     answers: ["var", "let", "const", "make"],
-    rightAnswer: 3 || index #,
+    rightAnswer: 3,
 }
 
-function goToQuestions() {
+const questionList = ["q1", "q2", "q3", "q4", "q5", "q6", "q7"]
+
+buttonEl.addEventListener("click", startGame)
+
+function startgame() {
+    showElement()
 }
 
-function gameTimer(), 
+function gameTimer()
     var countDown = setInterval(function() {
-    secondsLeft = secondsLeft -1;
+    secondsLeft = secondsLeft --;
     timeEl.textContent = "Time left:" + secondsLeft;
 
         if(secondsLeft === 0 ) {
@@ -66,9 +72,4 @@ function gameTimer(),
 
 function displayScore() {
 
-},
-// Below is going to add an event listener for a button click
-submitButton.addEventListener('click', goToQuestions),
-    submitButton
-    
-,console.log(var)
+}
