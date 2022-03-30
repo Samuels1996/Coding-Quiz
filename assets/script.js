e2f83d034fa9b0589b8b758c027568c1d7e3bc38
Brian Samuels <samuels.brian9@gmail.com>
2022-03-29 20:28:38 -0400

    Making additions to the css, index, as well as the js

--- assets/script.js
+++ assets/script.js
@@ -1,75 +1,88 @@
-var timeEl = document.querySelector(".time");
-var buttonEl = ('#button')
-// Below is just setting some base variables that will be used for the JS file
-var letsPlay = document.querySelector('#button');
-var currentScore = 0
-var secondsLeft = 75
-
-
 // Below is going to be the questions and answers for the quiz
-const q1 = {
+const questions = [
+
+  {
     question: "What CSS value acts as a frame around an image?",
     answers: ["Content", "Margin", "Border", "Padding"],
     rightAnswer: 2,
-}
+    },
 
-const q2 = {
+  {
     question: "Best practice for console logs is to ____ them to prevent hacking into generator devices", 
     answers: ["Delete", "Change", "Keep", "Realign"],
     rightAnswer: 0,
-}
+    },
 
-const q3 = {
+  {
     question: "To move a folder in git you must use",
     answers: ["rm", "mv", "pull", "mkdir"],
     rightAnswer: 1,
-}
+    },
 
-const q4 = {
+  {
     question: "What is the base star number for an index?",
     answers: ["-1", "1", "0", "32"],
     rightAnswer: 2,
 
-}
+    },
 
-const q5 = {
+  {
     question: "In JavaScript to determine if something is true or false is called a(n) ",
     answers: ["Array", "Case", "Parse", "Boolean"],
     rightAnswer: 3,
-}
+    },
 
-const q6 = {
+  {
     question: "In JavaScript you should use local storage for all personal information",
     answers: ["True", "False"],
     rightAnswer: 0,
-}
+    },
 
-const q7 = {
+  {
     question: "Which of these is not used to save a variable",
     answers: ["var", "let", "const", "make"],
     rightAnswer: 3,
+    }
+];
+
+// Below is just setting some base variables that will be used for the JS file
+var timeEl = document.getElementById("time");
+var playBtn = document.getElementById("play-btn");
+var questionContainerElement = document.getElementById("question-container");
+var questionsEl = document.getElementById("questions")
+let shuffledQuestions
+var currentScore = 0;
+var secondsLeft = 75;
+var questionList = 0;
+var penalty = 10;
+
+playBtn.addEventListener('click', startQuiz)
+
+
+
+console.log(playBtn)
+
+function startQuiz () {
+  playBtn.classList.setAttribute('hide')
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  showNextQuestion()
 }
 
-const questionList = ["q1", "q2", "q3", "q4", "q5", "q6", "q7"]
-
-buttonEl.addEventListener("click", startGame)
-
-function startgame() {
-    showElement()
-}
-
-function gameTimer()
-    var countDown = setInterval(function() {
-    secondsLeft = secondsLeft --;
-    timeEl.textContent = "Time left:" + secondsLeft;
-
-        if(secondsLeft === 0 ) {
-        clearInterval(countDown);
-        displayScore();
-        }
-    
-    },
-
-function displayScore() {
+function render(questionIndex) {
+  questionsDiv.innerHTML = "";
+  ulCreate.innerHTML = "";
+  for (var i = 0; i < questions.length; i++) {
+    var userQuestion = questions[questionIndex].title;
+    var userChoices = questions[questionIndex].choices;
+    questionsDiv.textContent = userQuestion
+  }
+  userChoices.forEach(function (newItem) {
+    var listItem = document.createElement("li");
+    listItem.textContent = newItem;
+    questionsDiv.appendChild(ulCreate);
+    ulCreate.appendChild(listItem);
+    listItem.addEventListener('click', (compare));
+  })
 
 }
